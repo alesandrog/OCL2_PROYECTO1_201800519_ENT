fb2cf09165ffe825167c0a6d0ae3b3a079af034f
Alesandro Gonzalez <nelsonalesandrog@gmail.com>
2020-10-27 23:40:08 -0600

    [feat] sentencias de control (do while)

---
+++ Proyecto1/src/app/Proyecto2/Instruccion/SentenciasControl/DoWhile.ts
@@ -0,0 +1,39 @@
+import { Instruccion } from "../../Abstract/Instruccion";
+import { Tipo, Tipos } from "../../Util/Tipo";
+import { Expresion } from "../../Abstract/Expresion";
+import { Entorno } from "../../TablaSimbolos/Entorno";
+import { Generator } from "../../Generator/Generator";
+import { Error_ } from "../../Util/Error_";
+
+export class DoWhile extends Instruccion {
+    private condicion: Expresion;
+    private instrucciones: Array<Instruccion> = new Array<Instruccion>();
+
+    constructor( condicion: Expresion, instrucciones: Array<Instruccion>, line: number, column: number) {
+        super(line, column);
+        this.condicion = condicion;
+        this.instrucciones = instrucciones;
+    }
+
+    compile(env: Entorno): void {
+        //TODO break y continue  
+        const generator = Generator.getInstance();
+        const trueLbl = generator.newLabel();
+        const falseLbl = generator.newLabel();
+        // Imprimir etiqueta sobre la que se hara el loop 
+        generator.addLabel(trueLbl);
+        const newEnv = new Entorno(env);
+        for(let i = 0; i < this.instrucciones.length; i++){
+            this.instrucciones[i].compile(newEnv);
+        }        
+        // Al ejecutar la condicion se generan ifs, etiquetas verdaderas y falsas
+        this.condicion.trueLabel = trueLbl;
+        this.condicion.falseLabel = falseLbl;
+        const condicion = this.condicion.compile(env);
+        if (condicion.type.type!= Tipos.BOOLEAN)
+            throw new Error_(this.line,this.column,'Semantico','Condicion no booleana');        
+        // Imprimir etiqueta verdadera con las instrucciones del if
+        generator.addLabel(falseLbl);        
+
+    }
+}
