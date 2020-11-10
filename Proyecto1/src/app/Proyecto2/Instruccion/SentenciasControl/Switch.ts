552a04bcadc96dcb96061ce0fe4aef6fb3724ffb
Alesandro Gonzalez <nelsonalesandrog@gmail.com>
2020-11-10 08:42:02 -0600

    [fix] saltos de salida en switch

--- Proyecto1/src/app/Proyecto2/Instruccion/SentenciasControl/Switch.ts
+++ Proyecto1/src/app/Proyecto2/Instruccion/SentenciasControl/Switch.ts
@@ -10,30 +10,45 @@ import { Igual } from '../../Expresion/Relacional/Igual';
 export class Switch extends Instruccion {
     private condicion: Expresion;
     private case_array: Array<Case> = new Array<Case>();
+    private def: Instruccion[] | null;
     private lbl_list : Array<string> = new Array<string>();
 
-    constructor( condicion: Expresion, case_array: Array<Case>, line: number, column: number) {
+    constructor( condicion: Expresion, case_array: Array<Case>, def: Instruccion[] | null, line: number, column: number) {
         super(line, column);
         this.condicion = condicion;
         this.case_array = case_array;
+        this.def = def;
     }
 
     compile(env: Entorno): void {
-        //TODO break y continue  
         const generator = Generator.getInstance();
         // Ejecutar expresion recibida en switch
         const leftOp = this.condicion.compile(env);
+        // Generar etiquetas para cada Case
         this.case_array.forEach((instr)=>{
             const exp = instr.condicion.compile(env);
             const caseLbl = generator.newLabel();
             this.lbl_list.push(caseLbl);           
             generator.addIf(leftOp.getValue() , exp.getValue(), "==", caseLbl );
         });
-        const exitLbl = generator.newLabel();        
+        const newEnv = new Entorno(env);
+        const exitLbl = generator.newLabel(); 
+        newEnv.break = exitLbl;       
+        const defLabel = generator.newLabel();
+        generator.addGoto(defLabel);     
         for(let i = 0; i < this.case_array.length; i++){
             generator.addLabel(this.lbl_list[i]);
-            this.case_array[i].compile(env);
-        }
+            this.case_array[i].compile(newEnv);
+        }                   
+        generator.addLabel(defLabel);
+        if(this.def !=null){
+            const defLbl = generator.newLabel();
+            generator.addLabel(defLbl);
+
+            this.def.forEach((instr)=>{
+                instr.compile(newEnv);
+            });
+        }                
         generator.addLabel(exitLbl);
     }
 }
