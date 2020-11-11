60e17343acb7b498ea19374286d3256eb5aea417
Alesandro Gonzalez <nelsonalesandrog@gmail.com>
2020-11-10 23:46:50 -0600

    [feat] operaciones relacionales (== !=)

--- Proyecto1/src/app/Proyecto2/Expresion/Relacional/Dif.ts
+++ Proyecto1/src/app/Proyecto2/Expresion/Relacional/Dif.ts
@@ -23,10 +23,10 @@ export class Dif extends Expresion{
         // Ejecutar operador izquierdo
         const left = this.left.compile(env);
         let right : Retorno | null = null;
-
+        right = this.right.compile(env);
         switch (left.type.type) {
             case Tipos.NUMBER:
-                right = this.right.compile(env);
+
                 switch (right.type.type) {
                     case Tipos.NUMBER:
                         // Generar etiquetas verdaderas y falsas
@@ -69,7 +69,144 @@ export class Dif extends Expresion{
                     return retorno;
                 }
                 break;
-        }
+                case Tipos.STRING:
+                    switch(right.type.type){
+                        case Tipos.STRING:
+                            // Generar etiquetas verdaderas y falsas
+                            this.trueLabel = this.trueLabel == '' ? generator.newLabel() : this.trueLabel;
+                            this.falseLabel = this.falseLabel == '' ? generator.newLabel() : this.falseLabel;
+
+                            // Generar etiqueta para simular loop
+                            const loopLbl = generator.newLabel();
+                            generator.addLabel(loopLbl);
+                            // Generar temporales para acceder a los valores en heap
+                            const heapLft = generator.newTemporal();
+                            const heapRgt = generator.newTemporal();
+                            // Etiqueta para ejecutar iteracion y salir de iteracion
+                            const itLbl = generator.newLabel();
+                            // Acceder a heap
+                            generator.addGetHeap(heapLft, left.getValue());
+                            generator.addGetHeap(heapRgt, right.getValue());                                
+                            // Salto condicional a etiqueta verdadera
+                            generator.addIf(heapLft, heapRgt, '==', itLbl);
+                            // Salto incondicional a etiqueta falsa 
+                            generator.addGoto(this.trueLabel);
+                            // Ejecutar iteracion
+                            generator.addLabel(itLbl);
+                            generator.addIf(heapLft, '-1', '==', this.falseLabel);
+                            generator.addExpression(left.getValue(), left.getValue(), 1,'+');
+                            generator.addExpression(right.getValue(), right.getValue(), 1,'+');                                
+                            generator.addGoto(loopLbl);
+
+                            
+                            // Retornar objeto booleano
+                            const retorno = new Retorno('', false, new Tipo(Tipos.BOOLEAN));
+                            // Setear etiquetas verdaderas y falsas
+                            retorno.trueLabel = this.trueLabel;
+                            retorno.falseLabel = this.falseLabel;
+                            return retorno;
+                        case Tipos.NULL:
+                            // Generar etiquetas verdaderas y falsas
+                            this.trueLabel = this.trueLabel == '' ? generator.newLabel() : this.trueLabel;
+                            this.falseLabel = this.falseLabel == '' ? generator.newLabel() : this.falseLabel;
+                            // Salto condicional a etiqueta verdadera
+                            generator.addIf(left.getValue(), right.getValue(), '!=', this.trueLabel);
+                            // Salto incondicional a etiqueta falsa 
+                            generator.addGoto(this.falseLabel);
+                            // Retornar objeto booleano
+                            const retorno2 = new Retorno('', false, new Tipo(Tipos.BOOLEAN));
+                            // Setear etiquetas verdaderas y falsas
+                            retorno.trueLabel = this.trueLabel;
+                            retorno.falseLabel = this.falseLabel;
+                            return retorno2;                                
+                    }
+
+            break;
+            case Tipos.ARRAY:
+                switch(right.type.type){
+                    case Tipos.ARRAY:
+                            // Generar etiquetas verdaderas y falsas
+                            this.trueLabel = this.trueLabel == '' ? generator.newLabel() : this.trueLabel;
+                            this.falseLabel = this.falseLabel == '' ? generator.newLabel() : this.falseLabel;
+                            // Salto condicional a etiqueta verdadera
+                            generator.addIf(left.getValue(), right.getValue(), '!=', this.trueLabel);
+                            // Salto incondicional a etiqueta falsa 
+                            generator.addGoto(this.falseLabel);
+                            // Retornar objeto booleano
+                            const retorno = new Retorno('', false, new Tipo(Tipos.BOOLEAN));
+                            // Setear etiquetas verdaderas y falsas
+                            retorno.trueLabel = this.trueLabel;
+                            retorno.falseLabel = this.falseLabel;
+                            return retorno;
+                    case Tipos.NULL:       
+                            // Generar etiquetas verdaderas y falsas
+                            this.trueLabel = this.trueLabel == '' ? generator.newLabel() : this.trueLabel;
+                            this.falseLabel = this.falseLabel == '' ? generator.newLabel() : this.falseLabel;
+                            // Salto condicional a etiqueta verdadera
+                            generator.addIf(left.getValue(), right.getValue(), '!=', this.trueLabel);
+                            // Salto incondicional a etiqueta falsa 
+                            generator.addGoto(this.falseLabel);
+                            // Retornar objeto booleano
+                            const retorno2 = new Retorno('', false, new Tipo(Tipos.BOOLEAN));
+                            // Setear etiquetas verdaderas y falsas
+                            retorno2.trueLabel = this.trueLabel;
+                            retorno2.falseLabel = this.falseLabel;
+                            return retorno2;
+
+                }
+            break;
+            case Tipos.TYPE:
+                switch(right.type.type){
+                    case Tipos.TYPE:
+                            // Generar etiquetas verdaderas y falsas
+                            this.trueLabel = this.trueLabel == '' ? generator.newLabel() : this.trueLabel;
+                            this.falseLabel = this.falseLabel == '' ? generator.newLabel() : this.falseLabel;
+                            // Salto condicional a etiqueta verdadera
+                            generator.addIf(left.getValue(), right.getValue(), '!=', this.trueLabel);
+                            // Salto incondicional a etiqueta falsa 
+                            generator.addGoto(this.falseLabel);
+                            // Retornar objeto booleano
+                            const retorno = new Retorno('', false, new Tipo(Tipos.BOOLEAN));
+                            // Setear etiquetas verdaderas y falsas
+                            retorno.trueLabel = this.trueLabel;
+                            retorno.falseLabel = this.falseLabel;
+                            return retorno;                            
+                    case Tipos.NULL:       
+                            // Generar etiquetas verdaderas y falsas
+                            this.trueLabel = this.trueLabel == '' ? generator.newLabel() : this.trueLabel;
+                            this.falseLabel = this.falseLabel == '' ? generator.newLabel() : this.falseLabel;
+                            // Salto condicional a etiqueta verdadera
+                            generator.addIf(left.getValue(), right.getValue(), '!=', this.trueLabel);
+                            // Salto incondicional a etiqueta falsa 
+                            generator.addGoto(this.falseLabel);
+                            // Retornar objeto booleano
+                            const retorno2 = new Retorno('', false, new Tipo(Tipos.BOOLEAN));
+                            // Setear etiquetas verdaderas y falsas
+                            retorno2.trueLabel = this.trueLabel;
+                            retorno2.falseLabel = this.falseLabel;
+                            return retorno2;
+                }
+                break;
+            case Tipos.NULL:
+                if(right.type.type == Tipos.NULL){
+                            // Generar etiquetas verdaderas y falsas
+                            this.trueLabel = this.trueLabel == '' ? generator.newLabel() : this.trueLabel;
+                            this.falseLabel = this.falseLabel == '' ? generator.newLabel() : this.falseLabel;
+                            // Salto condicional a etiqueta verdadera
+                            generator.addIf(left.getValue(), right.getValue(), '!=', this.trueLabel);
+                            // Salto incondicional a etiqueta falsa 
+                            generator.addGoto(this.falseLabel);
+                            // Retornar objeto booleano
+                            const retorno = new Retorno('', false, new Tipo(Tipos.BOOLEAN));
+                            // Setear etiquetas verdaderas y falsas
+                            retorno.trueLabel = this.trueLabel;
+                            retorno.falseLabel = this.falseLabel;
+                            return retorno;                        
+                }
+                break;
+
+
+            }
         throw new Error_(this.line, this.column, 'Semantico', `No se puede ${left.type.type} == ${right?.type.type}`);
     
     }
--- Proyecto1/src/app/Proyecto2/Expresion/Relacional/Igual.ts
+++ Proyecto1/src/app/Proyecto2/Expresion/Relacional/Igual.ts
@@ -4,6 +4,7 @@ import { Retorno } from "../../Util/Retorno";
 import { Generator } from "../../Generator/Generator";
 import { Tipos, Tipo } from "../../Util/Tipo";
 import { Error_ } from "../../Util/Error_";
+import { sequence } from '@angular/animations';
 
 export class Igual extends Expresion{
     private left: Expresion;
@@ -23,10 +24,9 @@ export class Igual extends Expresion{
         // Ejecutar operador izquierdo
         const left = this.left.compile(env);
         let right : Retorno | null = null;
-
+        right = this.right.compile(env);
         switch (left.type.type) {
             case Tipos.NUMBER:
-                right = this.right.compile(env);
                 switch (right.type.type) {
                     case Tipos.NUMBER:
                         // Generar etiquetas verdaderas y falsas
@@ -43,6 +43,7 @@ export class Igual extends Expresion{
                         retorno.falseLabel = this.falseLabel;
                         return retorno;
                     default:
+                        // ERROR
                         break;
                 }
             case Tipos.BOOLEAN:
@@ -69,6 +70,144 @@ export class Igual extends Expresion{
                     return retorno;
                 }
                 break;
+            case Tipos.STRING:
+                        switch(right.type.type){
+                            case Tipos.STRING:
+                                // Generar etiquetas verdaderas y falsas
+                                this.trueLabel = this.trueLabel == '' ? generator.newLabel() : this.trueLabel;
+                                this.falseLabel = this.falseLabel == '' ? generator.newLabel() : this.falseLabel;
+
+                                // Generar etiqueta para simular loop
+                                const loopLbl = generator.newLabel();
+                                generator.addLabel(loopLbl);
+                                // Generar temporales para acceder a los valores en heap
+                                const heapLft = generator.newTemporal();
+                                const heapRgt = generator.newTemporal();
+                                // Etiqueta para ejecutar iteracion y salir de iteracion
+                                const itLbl = generator.newLabel();
+                                // Acceder a heap
+                                generator.addGetHeap(heapLft, left.getValue());
+                                generator.addGetHeap(heapRgt, right.getValue());                                
+                                // Salto condicional a etiqueta verdadera
+                                generator.addIf(heapLft, heapRgt, '==', itLbl);
+                                // Salto incondicional a etiqueta falsa 
+                                generator.addGoto(this.falseLabel);
+                                // Ejecutar iteracion
+                                generator.addLabel(itLbl);
+                                generator.addIf(heapLft, '-1', '==', this.trueLabel);
+                                generator.addExpression(left.getValue(), left.getValue(), 1,'+');
+                                generator.addExpression(right.getValue(), right.getValue(), 1,'+');                                
+                                generator.addGoto(loopLbl);
+
+                                
+                                // Retornar objeto booleano
+                                const retorno = new Retorno('', false, new Tipo(Tipos.BOOLEAN));
+                                // Setear etiquetas verdaderas y falsas
+                                retorno.trueLabel = this.trueLabel;
+                                retorno.falseLabel = this.falseLabel;
+                                return retorno;
+                            case Tipos.NULL:
+                                // Generar etiquetas verdaderas y falsas
+                                this.trueLabel = this.trueLabel == '' ? generator.newLabel() : this.trueLabel;
+                                this.falseLabel = this.falseLabel == '' ? generator.newLabel() : this.falseLabel;
+                                // Salto condicional a etiqueta verdadera
+                                generator.addIf(left.getValue(), right.getValue(), '==', this.trueLabel);
+                                // Salto incondicional a etiqueta falsa 
+                                generator.addGoto(this.falseLabel);
+                                // Retornar objeto booleano
+                                const retorno2 = new Retorno('', false, new Tipo(Tipos.BOOLEAN));
+                                // Setear etiquetas verdaderas y falsas
+                                retorno.trueLabel = this.trueLabel;
+                                retorno.falseLabel = this.falseLabel;
+                                return retorno2;                                
+                        }
+
+                break;
+                case Tipos.ARRAY:
+                    switch(right.type.type){
+                        case Tipos.ARRAY:
+                                // Generar etiquetas verdaderas y falsas
+                                this.trueLabel = this.trueLabel == '' ? generator.newLabel() : this.trueLabel;
+                                this.falseLabel = this.falseLabel == '' ? generator.newLabel() : this.falseLabel;
+                                // Salto condicional a etiqueta verdadera
+                                generator.addIf(left.getValue(), right.getValue(), '==', this.trueLabel);
+                                // Salto incondicional a etiqueta falsa 
+                                generator.addGoto(this.falseLabel);
+                                // Retornar objeto booleano
+                                const retorno = new Retorno('', false, new Tipo(Tipos.BOOLEAN));
+                                // Setear etiquetas verdaderas y falsas
+                                retorno.trueLabel = this.trueLabel;
+                                retorno.falseLabel = this.falseLabel;
+                                return retorno;
+                        case Tipos.NULL:       
+                                // Generar etiquetas verdaderas y falsas
+                                this.trueLabel = this.trueLabel == '' ? generator.newLabel() : this.trueLabel;
+                                this.falseLabel = this.falseLabel == '' ? generator.newLabel() : this.falseLabel;
+                                // Salto condicional a etiqueta verdadera
+                                generator.addIf(left.getValue(), right.getValue(), '==', this.trueLabel);
+                                // Salto incondicional a etiqueta falsa 
+                                generator.addGoto(this.falseLabel);
+                                // Retornar objeto booleano
+                                const retorno2 = new Retorno('', false, new Tipo(Tipos.BOOLEAN));
+                                // Setear etiquetas verdaderas y falsas
+                                retorno2.trueLabel = this.trueLabel;
+                                retorno2.falseLabel = this.falseLabel;
+                                return retorno2;
+
+                    }
+                break;
+                case Tipos.TYPE:
+                    switch(right.type.type){
+                        case Tipos.TYPE:
+                                // Generar etiquetas verdaderas y falsas
+                                this.trueLabel = this.trueLabel == '' ? generator.newLabel() : this.trueLabel;
+                                this.falseLabel = this.falseLabel == '' ? generator.newLabel() : this.falseLabel;
+                                // Salto condicional a etiqueta verdadera
+                                generator.addIf(left.getValue(), right.getValue(), '==', this.trueLabel);
+                                // Salto incondicional a etiqueta falsa 
+                                generator.addGoto(this.falseLabel);
+                                // Retornar objeto booleano
+                                const retorno = new Retorno('', false, new Tipo(Tipos.BOOLEAN));
+                                // Setear etiquetas verdaderas y falsas
+                                retorno.trueLabel = this.trueLabel;
+                                retorno.falseLabel = this.falseLabel;
+                                return retorno;                            
+                        case Tipos.NULL:       
+                                // Generar etiquetas verdaderas y falsas
+                                this.trueLabel = this.trueLabel == '' ? generator.newLabel() : this.trueLabel;
+                                this.falseLabel = this.falseLabel == '' ? generator.newLabel() : this.falseLabel;
+/*                                // Accesar a heap
+                                const accesoHeap = generator.newTemporal();
+                                generator.addGetHeap(accesoHeap, left.getValue());*/
+                                // Salto condicional a etiqueta verdadera
+                                generator.addIf(left.getValue(), right.getValue(), '==', this.trueLabel);
+                                // Salto incondicional a etiqueta falsa 
+                                generator.addGoto(this.falseLabel);
+                                // Retornar objeto booleano
+                                const retorno2 = new Retorno('', false, new Tipo(Tipos.BOOLEAN));
+                                // Setear etiquetas verdaderas y falsas
+                                retorno2.trueLabel = this.trueLabel;
+                                retorno2.falseLabel = this.falseLabel;
+                                return retorno2;
+                    }
+                    break;
+                case Tipos.NULL:
+                    if(right.type.type == Tipos.NULL){
+                                // Generar etiquetas verdaderas y falsas
+                                this.trueLabel = this.trueLabel == '' ? generator.newLabel() : this.trueLabel;
+                                this.falseLabel = this.falseLabel == '' ? generator.newLabel() : this.falseLabel;
+                                // Salto condicional a etiqueta verdadera
+                                generator.addIf(left.getValue(), right.getValue(), '==', this.trueLabel);
+                                // Salto incondicional a etiqueta falsa 
+                                generator.addGoto(this.falseLabel);
+                                // Retornar objeto booleano
+                                const retorno = new Retorno('', false, new Tipo(Tipos.BOOLEAN));
+                                // Setear etiquetas verdaderas y falsas
+                                retorno.trueLabel = this.trueLabel;
+                                retorno.falseLabel = this.falseLabel;
+                                return retorno;                        
+                    }
+                    break;
         }
         throw new Error_(this.line, this.column, 'Semantico', `No se puede ${left.type.type} == ${right?.type.type}`);
     
